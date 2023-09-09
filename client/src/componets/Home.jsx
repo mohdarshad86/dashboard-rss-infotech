c3a366207508701385169a9503be5d6022ec4e10
Mohd Arshad <mohdarshad8668@gmail.com>
2023-09-09 20:34:54 +0530

    added Home component

--- client/src/componets/Home.jsx
+++ client/src/componets/Home.jsx
@@ -1,13 +1,57 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import styles from './Home.module.css';
+
+const BASE_URL = 'http://localhost:3001'
 
 function Home() {
+  // State variables to store counts
+  const [userCount, setUserCount] = useState([]);
+  const [offerCount, setOfferCount] = useState(0);
+  const [issueCount, setIssueCount] = useState(0);
 
-  useEffect(()=>{
-    
-  },[])
+  const getUsers = async () => {
+    const response = await axios.get(`${BASE_URL}/user`)
+
+    setUserCount(response.data.data);
+    // console.log(response.data.data);
+  }
+
+  const getOffers = async () => {
+    const response = await axios.get(`${BASE_URL}/offers`)
+
+    setOfferCount(response.data.data);
+  }
+
+  const getIssues = async () => {
+    const response = await axios.get(`${BASE_URL}/issues`)
+
+    setIssueCount(response.data.data);
+  }
+
+  useEffect(() => {
+    getUsers();
+    getOffers();
+    getIssues();
+  }, []);
+
   return (
-    <div>Home</div>
-  )
+    <div className={styles.cardContainer}>
+      <h1>Home</h1>
+      <div className={styles.card}>
+        <h2>Users</h2>
+        <p>Total Users: {userCount.length}</p>
+      </div>
+      <div className={styles.card}>
+        <h2>Offers</h2>
+        <p>Total Offers: {offerCount.length}</p>
+      </div>
+      <div className={styles.card}>
+        <h2>Issues</h2>
+        <p>Total Issues: {issueCount.length}</p>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
